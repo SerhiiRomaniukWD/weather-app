9bd99ce7d59d7cb3fc51eb7c24f6a7359c051aaf
serhii.dev <serhii.dev@serhiidevs-MacBook-Pro.local>
2025-04-07 15:24:38 +0200

    added tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import "./App.css";
-
 function App() {
-	return <h1>Hello, World!</h1>;
+	return <h1 className="text-2xl font-lexed">Hello, World!</h1>;
 }
 
 export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+export default {
+	mode: "jit",
+	content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+	theme: {
+		fontFamily: {
+			lexend: ["Lexend", "serif"],
+		},
+		colors: {
+			white: "#FFFFFF",
+			black: "#050505",
+		},
+		extend: {},
+	},
+	plugins: [],
+};
