bf4736e8148a8277408caa88eb6ebffb3958497c
serhii.dev <serhii.dev@serhiidevs-MacBook-Pro.local>
2025-04-07 17:47:08 +0200

    set up vite for svg

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
-import { defineConfig } from "vite";
+import { AliasOptions, defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
+import svgr from "vite-plugin-svgr";
+import { resolve } from "path";
+
+const root = resolve(__dirname, "src");
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), tailwindcss()],
+	plugins: [react(), tailwindcss(), svgr()],
+	resolve: {
+		alias: {
+			// ROOT
+			"@": resolve(root, "../"),
+			// ASSETS
+			"@icons": resolve(root, "assets/icons"),
+		} as AliasOptions,
+	},
 });
